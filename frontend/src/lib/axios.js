f462c5146d852e716053d08a3dec4988ed30fb76
Marufomer <omermaruf07@gmail.com>
2025-07-06 18:54:47 +0300

    updated

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // server Url
-const fallbackURL = "http://localhost:5003/api";
+const fallbackURL = "https://nile-intership-project.onrender.com/api";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL || fallbackURL,
